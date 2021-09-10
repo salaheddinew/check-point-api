327fa29c7993a320efe3043dcf7a853e32442947
salah <heba.salah400@gmail.com>
2021-09-10 22:55:38 +0100

    delete user comp

--- src/comp/UserLIste.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useEffect, useState } from "react";
-
-const UserLIste = () => {
-  const [liste, setliste] = useEffect();
-  return <div></div>;
-};
-
-export default UserLIste;
